4ba4ba3396a763b9becc556ea465fc427b47b97a
Marcos Gabriel <moliveirateixeira96@gmail.com>
2025-09-03 20:37:07 +0100

    Adding class commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,44 @@
 import express, { Request, Response } from "express"
+import {knex} from "./database/knex"
 
 const app = express()
 app.use(express.json())
 
-app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
+app.post("/courses", async (request: Request, response: Response) => {
+  const {name} = request.body
+
+  await knex("courses").insert({ name })
+
+  //await knex.raw("INSERT INTO courses (name) VALUES (?)", [name] )
+  
+  response.status(201).json()
 })
+
+app.get("/courses", async (request: Request, response: Response) => {
+  //const courses = await (knex.raw("SELECT * FROM courses"))
+  const courses = await knex("courses").select().orderBy("id")
+  
+  response.status(200).json(courses)
+})
+
+app.put("/courses", async (request: Request, response: Response) => {
+  const {name} = request.body
+  const {id}  = request.params
+
+  await knex("courses").update({name}).where({id})
+  response.json()
+})
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const {name} = request.body
+  const {id}  = request.params
+
+  await knex("courses").delete().where({id})
+  
+  response.json()
+})
+
+
+
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
